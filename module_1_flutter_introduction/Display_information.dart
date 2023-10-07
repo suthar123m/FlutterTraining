f91020aa3fd866272853c475537b4a9fa147509d
Mahendra Kumar <140679549+suthar123m@users.noreply.github.com>
2023-10-07 15:16:29 +0530

    updated

---
+++ module_1_flutter_introduction/Display_information.dart
@@ -0,0 +1,7 @@
+void main()
+{
+  print("My name is Mahendra Kumar.");
+  print("My birth date is 6-july-2000.");
+  print("My age is 23 year old.");
+  print("i am live in Sumerpur,Pali(Rajasthan).");
+}
